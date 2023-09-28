da73e135563713fd0f7ee333fd28fa83d88e5cb0
stase-dima <d.stasenko@mirtekgroup.ru>
2023-09-28 15:17:13 +0300

    Added function for get element by id, fixed count_list_items and item_data functions

--- Task1.c
+++ Task1.c
@@ -8,18 +8,40 @@ list_t;
 	/* Counts the number of items in a list.
 	 */
 int count_list_items(const list_t *head) {
-	if (head->next) {
-		return count_list_items(head->next) + 1;
-	} else {
-		return 1;
-	}
+    list_t *current_item = head;
+    int num_of_elements = 1;
+    while (current_item->next)
+    {
+        current_item = current_item->next;
+        num_of_elements++;
+    }
+    return num_of_elements;
+}
+
+	/* Getting element inside the list by index.
+	 */
+int get_item_by_index(const list_t *head, const int index) {
+    list_t *current_item = head;
+    int number_of_element = 0;
+    while (current_item)
+    {
+        if (number_of_element == index)
+        {
+            return current_item->data;
+        }
+        current_item = current_item->next;
+        number_of_element++;
+    }
+    return 0;
 }
 
     /* Inserts a new list item after the one specified as the argument.
 	 */
 void insert_next_to_list(list_t *item, int data) {
     // memory for next element of list
+    list_t *old_next_element = item->next;
     item->next = malloc(sizeof(list_t));
+    item->next->next = old_next_element;
     item->next->data = data;    
 }
 
@@ -37,20 +59,8 @@ void remove_next_from_list(list_t *item) {
 	 */
 char *item_data(const list_t *list)
 {
-	char buf[12];
+	char *buf = malloc(count_list_items(list));
 
-    int num_of_elements = count_list_items(list);
-    const list_t *next_list_item = list->next;
-    buf[0] = (char)list->data;
-    for (int i = 1; (i < num_of_elements && i < 12); i++)
-    {
-        buf[i] = next_list_item->data;
-        if (!next_list_item->next) {
-            break;
-        }
-        else {
-            next_list_item = next_list_item->next;
-        }
-    }
+	sprintf(buf, "%d", list->data);
 	return buf;
 }
